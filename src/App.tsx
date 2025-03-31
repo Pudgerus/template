adcd9aa06dd2e068d62291d33789cec9bf847f71
Pudgerus <114077090+Pudgerus@users.noreply.github.com>
2025-03-31 15:44:13 +0300

    task done?

--- src/App.tsx
+++ src/App.tsx
@@ -21,3 +21,31 @@ import React from 'react';
 // }
 
 // export default App;
+
+
+function App() {
+    return (
+        <div className="App">
+            <header>
+                <span>
+                    <h3><a href="#">SPECTRUM</a></h3>
+                    <p>+7(4567) 360-76-80 <br />email@yandex.ru</p>
+                </span>
+                <h3><a href="#">MENU</a></h3>
+            </header>
+            <div className="stick"></div>
+            <div className="content">
+                <h1>WE WILL SHOW <br /> YOUR <br /> TALENTS</h1>
+                <p>As result we help our students to reveal hidden talents, find their true calling and lifework that will never disapiont them</p>
+            </div>
+            <div className="selector">
+                <p><a href="#">ABOUT COMPANY</a></p>
+                <p><a href="#">COMENTS</a></p>
+                <p><a href="#">NEWS</a></p>
+            </div>
+
+        </div>
+    );
+}
+
+export default App;
